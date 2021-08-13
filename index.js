44176e8cdb417f58197cf003c77d53b1bc39c044
pranik chainani <pranikc@gmail.com>
2021-08-13 01:37:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, occasion) {
+    let res = []
+    for(let i = 0; i < names.length; i++){
+        res.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+    }
+    return res
+}
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num)
+        num--
+    }
+}
